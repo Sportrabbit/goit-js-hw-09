0dd106a33d4069d9336498b57cf846c10017973c
Sportrabbit <145197781+Sportrabbit@users.noreply.github.com>
2024-01-30 19:19:29 +0200

    Update 1-gallery.js

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const images = [
